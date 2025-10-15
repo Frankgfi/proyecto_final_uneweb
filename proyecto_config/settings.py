307846853172170ccd10c5951fc4bf87941971bc
unknown <gefi0998@gmail.com>
2025-10-15 13:03:06 -0400

    Allowed hosts modificado

--- proyecto_config/settings.py
+++ proyecto_config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-s81qg7z)+q4&q8rfpy7=1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '.onrender.com').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'inventario-django-86nw.onrender.com').split(',')
 
 CSRF_TRUSTED_ORIGINS = [
     f"https://{os.environ.get('RENDER_EXTERNAL_HOSTNAME', '')}"
